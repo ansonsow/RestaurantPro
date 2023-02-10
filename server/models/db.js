1944586e55558a522128e9952fc2bc246d0bdd25
ansonsow <60858599+ansonsow@users.noreply.github.com>
2023-02-09 19:27:25 -0800

    build: db setup

--- server/models/db.js
+++ server/models/db.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", true);
+
+// const dbURL = 'mongodb+srv://group_3:5NTH2XswpIvMKA5Oa@cluster0.49zw62h.mongodb.net/?retryWrites=true&w=majority';
+
+const dbURL = 'mongodb+srv://group_3:5NTH2XswpIvMKA5Oa@restaurantpro.kzv4u9s.mongodb.net/?retryWrites=true&w=majority'
+
+// mongodb+srv://group_3:<password>@restaurantpro.kzv4u9s.mongodb.net/?retryWrites=true&w=majority
+
+mongoose.connect(dbURL, { useNewUrlParser: true})
+
+// console.log("wtf");
+mongoose.connection.on('connected',()=>{
+    console.log(`Mongoose connection to ${dbURL}`);
+})
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+});
+
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+    
