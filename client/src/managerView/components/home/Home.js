d3a2ce893e8dc2cf04a657734d84a23e45950965
HT <rhizodont@gmail.com>
2023-03-19 20:14:05 -0700

    style: manager: quick actions restyle (text only)

--- client/src/managerView/components/home/Home.js
+++ client/src/managerView/components/home/Home.js
@@ -1,35 +1,35 @@
 import React from "react";
 import "./Home.css";
+// import PersonalDetails from "./personalDetails/PersonalDetails";
+import QuickActions from "./quickactions/QuickActions";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="assign-task-page-upper-section-button">
-      <h3>Quick Actions</h3>
-      <div className="links">
-        <Link to="/create-task" className="link-a">
-          <button>Create New Task</button>
-        </Link>
-        <Link to="/task" className="link-a">
-          <button> See All Task</button>
-        </Link>
-        <Link to="/daily-attendance" className="link-a">
-          <button>Attendance Scree</button>
-        </Link>
-      </div>
+    <div className="home_page manager_view">
+      <div className="board">
+        <h2>Quick Actions</h2>
 
-      <div className="links">
-        <Link to="/assign-task" className="link-a">
-          <button>Assign Task</button>
-        </Link>
-        <Link to="/employee" className="link-a">
-          <button>Employee List</button>
-        </Link>
-        <Link to="/daily-attendance" className="link-a">
-          <button>Check Daily Attendance</button>
-        </Link>
-      </div>
+        <div className="quick_actions">
+          <Link to="/create-task">
+            <span>Create New Task</span>
+          </Link>
+          <Link to="/task">
+            <span>See All Tasks</span>
+          </Link>
+          <Link to="/daily-attendance">
+            <span>Check Daily Attendance</span>
+          </Link>
+          <Link to="/assign-task">
+            <span>Assign Task</span>
+          </Link>
+          <Link to="/employee">
+            <span>See Employee List</span>
+          </Link>
+        </div>
+      </div>{/* end .board */}
 
     </div>
+    // end .manager_view
   );
 }
