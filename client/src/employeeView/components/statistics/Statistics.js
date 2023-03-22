663b5f86d22586cb61481f453bc9b8b5c66c3ed4
HT <rhizodont@gmail.com>
2023-03-21 23:52:29 -0700

    build: employee: started pages: [stats, notifs, help]

---
+++ client/src/employeeView/components/statistics/Statistics.js
@@ -0,0 +1,12 @@
+import "./Statistics.css";
+
+const Statistics = () => {
+
+    return(
+        <div className="stats_page">
+            <p>stats page.</p>
+        </div>
+    )
+}
+
+export default Statistics;
