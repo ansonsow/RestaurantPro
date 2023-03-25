7af74a8a91f20e1481ed9e36456a1e2c31c29fd0
ansonsow <60858599+ansonsow@users.noreply.github.com>
2023-03-24 23:36:11 -0700

    trimmed due dates in task

--- client/src/managerView/components/Tasks/AssignTasks/EmployeeAssignedTask.js
+++ client/src/managerView/components/Tasks/AssignTasks/EmployeeAssignedTask.js
@@ -4,13 +4,16 @@ function EmployeeAssignedTask(props) {
   let dueDate = new Date();
   // default to 3 hours after now
   dueDate.setHours(dueDate.getHours()+3);
-  console.log(props.task.due_date);
-  dueDate.toString();
+  // console.log(props.task.due_date);
+  let strDueDate = dueDate.toString();
+
+  let trimmed = strDueDate.substring(0,21)
+
   return (
     <tr>
       <td>{props.task.task_name}</td>
-      <td>{props.task.due_date}</td>
-      {/* <td>{dueDate.toString()}</td> */}
+      {/* <td>{props.task.due_date}</td> */}
+      <td>{trimmed}</td>
 
       <td>{props.task.priority === 1 ? "high" : "low"}</td>
     </tr>
