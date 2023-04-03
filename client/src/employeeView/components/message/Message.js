bc17c2d193e62b16e0ddd0be47c40af8f7130c23
HT <rhizodont@gmail.com>
2023-04-02 22:41:15 -0700

    style: employee: popups: more unified

--- client/src/employeeView/components/message/Message.js
+++ client/src/employeeView/components/message/Message.js
@@ -13,19 +13,34 @@ function Message(props) {
   //   }, 1000);
   // }, [message]);
 
+  let popupSpeed_ = getComputedStyle(document.documentElement).getPropertyValue("--Popup-Msg-Box-Fade-Speed");
+  popupSpeed_ = Number(popupSpeed_.replace(/[^\d.]*/g,""));
+
   const changeStatus = () => {
-    props.showMessage(false);
-    props.getUserTasksIds(localStorage.getItem("userId"));
+    // props.showMessage(false);
+    // props.getUserTasksIds(localStorage.getItem("userId"));
+    document.querySelector(".ht_popup").classList.remove("fade-in-css-ani");
+    document.querySelector(".ht_popup").classList.add("fade-out-css-ani")
+
+    setTimeout(() => {
+      props.showMessage(false);
+      props.getUserTasksIds(localStorage.getItem("userId"));
+    },popupSpeed_)
     // props.setShowBoard(true);
   };
   return (
     <>
       {console.log("in message")}
       {message && (
-        <div className="message">
-          <h4>{props.heading}</h4>
-          <h5>{props.message}</h5>
-          <button onClick={changeStatus}>Ok</button>
+        <div className="ht_popup fade-in-css-ani pop_always_show">
+          <div className="ht_popup_box">
+            <h3>{props.heading}</h3>
+            <h3>{props.message}</h3>
+            
+            <div className="ht_popup_buttons">
+              <button className="btn_okay btn_always_show" onClick={changeStatus}>Okay</button>
+            </div>
+          </div>
         </div>
       )}
     </>
